5d24c1de46ce0ddf3b52e5974d043f00d23b06f2
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-11-20 08:16:09 -0600

    feat: schema is ready

--- schema.js
+++ schema.js
@@ -6,6 +6,81 @@ const {
   GraphQLList,
   GraphQLNonNull
 } = require("graphql");
-const axios = require('axios');
+const axios = require("axios");
 
 // note type
+const NoteType = new GraphQLObjectType({
+  name: "Note",
+  fields: () => ({
+    title: { type: GraphQLString },
+    body: { type: GraphQLString },
+    author: { type: GraphQLString },
+    created_at: { type: GraphQLString }
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQuery",
+  fields: {
+    notes: {
+      type: new GraphQLList(NoteType),
+      resolve(parentValue, args) {
+        return axios.get('http://localhost:3000/notes')
+        .then(notes => notes.data)
+      }
+    }
+  }
+});
+
+const mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addNote: {
+      type: NoteType,
+      args: {
+        title: { type: new GraphQLNonNull(GraphQLString) },
+        body: { type: new GraphQLNonNull(GraphQLString) },
+        author: { type: new GraphQLNonNull(GraphQLString) },
+        created_at: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        return axios.post("http://localhost:3000/notes", {
+          title: args.title,
+          body: args.body,
+          author: args.author,
+          created_at: new Date()
+        })
+        .then(note => note.data);
+      }
+    },
+    editNote: {
+      type: NoteType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        title: { type: GraphQLString },
+        body: { type: GraphQLString },
+        author: { type: GraphQLString },
+        created_at: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        return axios.put(`http://localhost:3000/notes/${args.id}`, args)
+        .then(notes => notes.data);
+      }
+    },
+    deleteNote: {
+      type: NoteType,
+      args: { 
+        id: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve(parentValue, args) {
+        return axios.delete(`http://localhost:3000/notes/${args.id}`)
+        .then(notes => notes.data);
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+  mutation: mutation
+});
